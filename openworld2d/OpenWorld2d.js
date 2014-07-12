72a8f3d83b62ce4407357664425142af8c000a44
Filip Smets <alemaaltevinden@gmail.com>
2014-07-12 22:37:44 +0200

    Make OpenWorld2d strictly those things that can be shared on the server and the client: the update and the data about the world

--- openworld2d/OpenWorld2d.js
+++ openworld2d/OpenWorld2d.js
@@ -1,74 +1,28 @@
-var OpenWorld2d = (function () {
-	/*
-		OpenWorld2d Constructor
-	*/
-	function OpenWorld2d (settings) {
-		var defaultSettings = {
-			drawing: {},
-			update: {
-				tickTime: 1000 / 20
-			},
-			network: {
-				server: "http://defaultserver.com"
-			}
-		};
-		
-		this.settings = this.normalizeDefaults(settings, defaultSettings);
-		this.data = {};
-		this.saveListeners = [];
-		
-		if (typeof document !== "undefined" && typeof document.createElement === "function") {
-			this.screen = new Screen(this.settings, this.data);
+function OpenWorld2d (container, settings) {
+	this.settings = this.normalizeDefaults(settings, this.defaultSettings);
+	this.heightMap = new HeightMap(settings.map);
+}
+
+OpenWorld2d.prototype.defaultSettings = {
+	map: {},
+	tickTime: 1000 / 20
+};
+
+OpenWorld2d.prototype.normalizeDefaults = function normalizeDefaults (target, defaults) {
+	var normalized = {};
+	for (var k in defaults) {
+		if (typeof defaults[k] === "object") {
+			normalized[k] = this.normalizeDefaults(target[k], defaults[k]);
+		} else {
+			normalized[k] = target[k] || defaults[k];
 		}
 	}
-	
-	OpenWorld2d.prototype.normalizeDefaults = function normalizeDefaults (target, defaults) {
-		var normalized = {};
-		for (var k in defaults) {
-			if (typeof defaults[k] === "object") {
-				normalized[k] = this.normalizeDefaults(target[k], defaults[k]);
-			} else {
-				normalized[k] = target[k] || defaults[k];
-			}
-		}
-		return normalized;
-	};
+	return normalized;
+};
 
-	OpenWorld2d.prototype.updateWorld = updateWorld () {
-		
-	};
-
-	OpenWorld2d.prototype.listenToSave = function listenToSave (callback) {
-		if (typeof callback !== "function") return;
-		this.saveListeners.push(callback);
-	};
-
-	OpenWorld2d.prototype.save = function save (callback) {
-		var errs = [],
-		numberOfCallbacks = this.saveListeners.length,
-		calledBack = 0;
-		for (var fn = 0; fn < this.saveListeners.length; fn++) {
-			this.saveListeners[fn](this.data, function (err) {
-				if (err) errs.push(err);
-				calledBack++;
-				if (numberOfCallbacks <= calledBack) callback(errs);
-			});
-		}
-	};
+OpenWorld2d.prototype.updateWorld = updateWorld (deltaTime) {
 	
-	/*
-		Screen constructor
-	*/
-	
-	function Screen (settings, data) {
-		this.settings = settings;
-		this.data = data;
-		this.canvas = this.settings.drawing.canvas || document.createElement("canvas");
-		this.ctx = this.canvas.getContext("2d");
-	}
-	
-	return OpenWorld2d;
-})();
+};
 
 if (module && module.exports) {
 	module.exports = OpenWorld2d;
