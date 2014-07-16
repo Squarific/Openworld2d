c801dc72c4cacf582c539bbce519c0a375b3e7c8
Filip Smets <alemaaltevinden@gmail.com>
2014-07-16 22:23:19 +0200

    Fixed bug with no default seed + add shiftHeight function

--- openworld2d/HeightMap.js
+++ openworld2d/HeightMap.js
@@ -1,7 +1,7 @@
 function HeightMap (settings) {
 	this.settings = utils.normalizeDefaults(settings, this.defaultSettings);
 	if (Perlin && this.settings.noiseType === "perlin") {
-		this.noise = new Perlin(settings.seed).simplex2;
+		this.noise = new Perlin(this.settings.seed).simplex2;
 	} else if (typeof this.settings.noiseFunction === "function") {
 		this.noise = this.settings.noiseFunction;
 	} else {
@@ -10,13 +10,16 @@ function HeightMap (settings) {
 }
 
 HeightMap.prototype.getHeight = function getHeight (x, y) {
-	return this.noise(x / this.settings.zoomX, y / this.settings.zoomY);
+	return this.settings.shiftHeight(this.noise(x / this.settings.zoomX, y / this.settings.zoomY));
 };
 
 HeightMap.prototype.defaultSettings = {
 	seed: Math.random(),
 	zoomX: 500,
 	zoomY: 500,
+	shiftHeight: function (height) {
+		return height * height * height + 0.02;
+	},
 	noiseType: "perlin"
 };
 
